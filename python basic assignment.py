09fb1b28c3037682fd212575ae8cb62763a46b0e
VeereshKanuri <106917784+VeereshKanuri@users.noreply.github.com>
2023-05-10 21:22:03 +0530

    Add files via upload

---
+++ python basic assignment.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #1
+# 
+# Python programming language was developed by Guido van Rossum in the late 1980s and was first released in 1991.
+
+# #2
+# Python supports various programming paradigms including procedural, object-oriented, and functional programming. It also provides support for interactive and dynamic programming
+
+# #3
+# yes python is case sensitive when dealing with identifiers
+
+# #4
+# .py file 
+
+# #5
+# python code is interpreted
+
+# #6
+# In Python, we can define several blocks of code using the following:
+# 
+# Functions: A function is a block of code that performs a specific task. It helps to organize code into smaller, more manageable pieces. Functions can be defined using the def keyword followed by the function name and parameters, and then the block of code within the function.
+# 
+# Classes: A class is a blueprint for creating objects. It defines a set of attributes and methods that describe the object's properties and behavior. Classes can be defined using the class keyword followed by the class name and a colon, then the class definition.
+# 
+# Loops: A loop is used to repeat a block of code multiple times. In Python, we have two types of loops: for loop and while loop. The for loop is used to iterate over a sequence, while the while loop is used to repeat a block of code as long as a condition is true.
+# 
+# Conditional statements: Conditional statements are used to execute specific blocks of code based on certain conditions. In Python, we have the if, elif, and else statements. The if statement is used to check a condition and execute a block of code if it is true. The elif statement is used to check additional conditions if the previous conditions are false. The else statement is used to execute a block of code if none of the previous conditions are true.
+# 
+# Try-Except blocks: A try-except block is used to handle errors and exceptions that may occur during program execution. The try block contains the code that may cause an exception, while the except block contains the code to handle the exception.
+
+# #7
+# 
+# In Python, the hash symbol (#) is used to give single line comments. Any text written after the hash symbol is ignored by the Python interpreter
+
+# #8
+# sys.version: This function returns the version of Python interpreter as a string.
+# 
+# platform.python_version(): This function returns the version of Python interpreter as a string along with additional platform information.
+# 
+# sys.version_info: This function returns the version of Python interpreter as a tuple of integers, where each integer represents a component of the version number (major, minor, micro, releaselevel, and serial).
+# 
+# platform.python_implementation(): This function returns the name of the Python implementation (CPython, Jython, IronPython, PyPy, etc.).
+
+# #9
+# 
+# lambda functions.
+
+# #10
+# 
+# pip stands for "pip installs packages" or "Python package installer". It is a command-line tool for installing, managing, and removing Python packages and their dependencies.
+
+# #11
+# print() - used to print output to the console
+# input() - used to accept user input
+# len() - used to find the length of a string, list, or other iterable
+# str() - used to convert an object to a string
+# int() - used to convert a string or float to an integer
+# float() - used to convert a string or integer to a float
+# type() - used to find the data type of an object
+# range() - used to generate a sequence of numbers
+# zip() - used to combine multiple iterables into a single iterable of tuples
+# sum() - used to find the sum of elements in an iterable
+
+# #12
+# The maximum possible length of an identifier (variable name, function name, etc.) in Python is technically unlimited. However, it is generally recommended to limit the length of identifiers for the sake of readability and maintainability of the code. PEP 8, the official Python style guide, recommends limiting the length of identifiers to 79 characters or less
+
+# #13
+# Easy to learn and use: Python has a simple syntax and is easy to learn for beginners. It has a large and supportive community, which makes it easy to find help and resources.
+# 
+# Versatile: Python can be used for a wide range of applications such as web development, data analysis, machine learning, scientific computing, artificial intelligence, and more.
+# 
+# Cross-platform compatibility: Python code can run on different operating systems such as Windows, Mac, and Linux.
+# 
+# Large standard library: Python comes with a large number of built-in modules and functions that can be used for various tasks, which reduces the amount of code required for a
+
+# #14
+# 
+# Memory management in Python is done through a private heap space that contains all Python objects and data structures. Python's memory management is handled by a system of reference counting, garbage collection, and memory allocation.
+# 
+# In Python, every object created is given a unique reference count that keeps track of how many references are pointing to it. When an object's reference count reaches zero, it is considered unused and is immediately deallocated from the memory by the garbage collector. The garbage collector is a mechanism in Python that automatically frees up memory that is no longer being used by the program.
+# 
+# Python also provides dynamic memory allocation through its built-in data structures such as lists, dictionaries, and tuples. These data structures automatically allocate and deallocate memory as needed, making it easier for programmers to write efficient code without worrying too much about memory management.
+# 
+# Overall, Python's memory management system is designed to be simple and easy to use, allowing developers to focus on writing code without worrying too much about low-level memory management details.
+
+# #15
+# Download the latest version of Python from the official website https://www.python.org/downloads/ and run the installer.
+# 
+# During the installation process, make sure to select the option to add Python to PATH.
+# 
+# Once the installation is complete, you can test if Python is installed correctly by opening the Command Prompt and typing "python" to start the Python interpreter.
+# 
+# To set the path variables, you need to follow these steps:
+# 
+# Right-click on "My Computer" or "This PC" and select "Properties".
+# Click on "Advanced system settings".
+# Click on the "Environment Variables" button.
+# Under "System Variables", find "Path" and click "Edit".
+# Click "New" and add the path to the Python installation directory (e.g., C:\Python39).
+# Yes, indentation is required in Python as it is used to define blocks of code. Unlike other programming languages, which use braces or keywords to define code blocks, Python uses indentation to indicate the start and end of a block. The standard convention is to use four spaces for indentation. Incorrect indentation can result in syntax errors or unexpected behavior.
+# 
+# 
+# 
+# 
+# 
+# 
+# 
